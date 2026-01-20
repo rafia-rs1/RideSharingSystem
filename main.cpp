53a8fc72850754443ad31aeb8008d5f81d3b7926
Rafia Rehan <f2024332032@umt.edu.pk/login>
2026-01-20 21:33:26 +0500

    New Files (Driver)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "City.h"
+#include "Driver.h"
 
 int main() {
     City c("Lahore");
@@ -9,5 +10,15 @@ int main() {
 
     c.displayLocations();
 
+    Driver d1(1, "Ali", "DHA");
+
+    d1.display();
+
+    d1.assignTrip();
+    d1.display();
+
+    d1.completeTrip();
+    d1.setLocation("Gulberg");
+    d1.display();
     return 0;
 }
