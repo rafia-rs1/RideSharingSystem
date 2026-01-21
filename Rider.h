c8823b0aeab87c51c1cfb2abc257a4077ff94a65
Rafia Rehan <f2024332032@umt.edu.pk/login>
2026-01-22 01:27:28 +0500

    New Files (Rider.h , Rider.cpp

---
+++ Rider.h
@@ -0,0 +1,19 @@
+// ================= Rider.h =================
+#ifndef RIDER_H
+#define RIDER_H
+#include <string>
+using namespace std;
+
+
+class Rider {
+public:
+int id;
+string name;
+
+
+Rider(int i, string n);
+void display();
+};
+
+
+#endif
